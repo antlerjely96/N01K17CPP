98e0c701591c1c31481385d83f06a9b744546977
antlerjely96 <antlerjely96@gmail.com>
2025-10-11 10:52:54 +0700

    demo Condition bai4

--- Buoi2/Bai4.cpp
+++ Buoi2/Bai4.cpp
@@ -5,3 +5,26 @@
 	Neu diem >= 6.5 && diem < 8 => Kha
 	Con lai => gioi
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao bien
+	double diem;
+	//Nhap diem
+	cout << "diem = ";
+	cin >> diem;
+	//Kiem tra diem
+	if(diem < 0){
+		cout << "Nhap sai" << endl;
+	} else if (diem < 5){
+		cout << "yeu" << endl;
+	} else if(diem < 6.5){
+		cout << "trung binh" << endl;
+	} else if(diem < 8){
+		cout << "kha" << endl;
+	} else {
+		cout << "gioi" << endl;
+	}
+	return 0;
+}
