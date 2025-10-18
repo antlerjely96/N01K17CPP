ed078419ec837f2662517db22753cb8ecb6a62b5
antlerjely96 <antlerjely96@gmail.com>
2025-10-18 08:29:09 +0700

    demo Condition bai6

--- Buoi2/Bai6.cpp
+++ Buoi2/Bai6.cpp
@@ -2,3 +2,43 @@
 	6. Nhap 3 so nguyen a, b, c.
 	Hien thi so lon nhat va so nho nhat trong 3 so
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao a, b, c
+	int a;
+	int b;
+	int c;
+	//Nhap a, b, c
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	//Tim max
+	int max = a;
+	//Kiem tra
+	if(max < b){
+		max = b;
+	}
+	if(max < c){
+		max = c;
+	}
+	//Hien thi max
+	cout << "max = " << endl;
+	//Tim min
+	int min = a;
+	//Kiem tra
+	if(min > b){
+		min = b;
+	}
+	if(min > c){
+		min = c;
+	}
+	//Hien thi min
+	cout << "min = " << endl;
+	return 0;
+}
