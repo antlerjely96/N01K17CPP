8e8c55c66b213d5ba200e2bf692f52684f0649f3
antlerjely96 <antlerjely96@gmail.com>
2025-11-08 11:54:13 +0700

    Loop BTVN

--- Buoi5/Bai20.cpp
+++ Buoi5/Bai20.cpp
@@ -1,4 +1,14 @@
 /*
-	21. Nhap so nguyen n tu ban phim.
+	20. Nhap so nguyen n tu ban phim.
 	Hien thi n so dau tien cua day fibonacci
+	Fibonacci: so dung sau bang tong 2 so dung truoc
+	21. Nhap so nguyen n. Tinh
+	S = 1/1 + 1/2 + 1/3 + ... + 1/n
+	22. Nhap 2 so nguyen tu ban phim.
+	Tim uoc chung lon nhat va boi chung nho nhat cua 2 so
+	23. Nhap so nguyen n tu ban phim.
+	Hien thi cac so trong khoang tu 1 den n thoa man dieu kien:
+		so do chia het cho lan luot tung chu so cua no
+	Vi du: n = 50
+		=> Hien thi: 1 2 3 4 5 6 7 8 9 11 12 15 22 24 33 36 44 48
 */
