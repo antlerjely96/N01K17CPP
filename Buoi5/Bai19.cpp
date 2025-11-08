47dcae7875e278d1143b5280dcc05505a7020c3a
antlerjely96 <antlerjely96@gmail.com>
2025-11-08 10:49:53 +0700

    Loop Bai 19

--- Buoi5/Bai19.cpp
+++ Buoi5/Bai19.cpp
@@ -5,3 +5,27 @@
 	11	12	13	14	15
 	20	19	18	17	16
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao
+	int j, k;
+	for(int i = 0; i < 4; i++){
+		//So dau dong
+		j = 1 + i * 5;
+		//So cuoi dong
+		k = 5 + i * 5;
+		if(i % 2 == 0){
+			for(int n = j; n <= k; n++){
+				cout << n << "\t";
+			}
+		} else {
+			for(int n = k; n >= j; n--){
+				cout << n << "\t";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
