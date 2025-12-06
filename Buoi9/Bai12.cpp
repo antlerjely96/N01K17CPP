8fb7ab4f73233bdcb586fce55558518aff1ebd54
antlerjely96 <antlerjely96@gmail.com>
2025-12-06 10:46:32 +0700

    Array Bai12

--- Buoi9/Bai12.cpp
+++ Buoi9/Bai12.cpp
@@ -2,3 +2,34 @@
 	12. Nhap mang so nguyen co n phan tu.
 	Hien thi tat ca cac phan tu la so nguyen to trong mang
 */
+#include <iostream>
+using namespace std;
+
+int main(){
+	// khai bao bien
+	int n;
+	cout <<"Nhap so phan tu: ";
+	cin >> n;
+	// khai bao mang
+	int arr[n];
+	cout <<"Nhap " << n << " phan tu n: \n";
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	} 
+	//kiem tra
+	cout <<"Cac so nguyen to trong mang: ";
+	for(int i = 0; i < n; i++){
+		int demUoc = 0;
+		for(int j = 1; j <= arr[i]; j++){
+			if(arr[i] % j == 0){
+				demUoc++;
+			}
+		}
+		cout << demUoc << endl;
+		if(demUoc == 2){
+			cout << arr[i] <<" ";
+		}
+	}
+	return 0;
+}
