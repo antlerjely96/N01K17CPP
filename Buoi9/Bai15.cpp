ddeec523ea0c458632fa082e7e8e6b5a95138c60
antlerjely96 <antlerjely96@gmail.com>
2025-12-13 08:47:22 +0700

    Array Bai15

--- Buoi9/Bai15.cpp
+++ Buoi9/Bai15.cpp
@@ -2,3 +2,45 @@
 	15. Nhap mang so nguyen co n phan tu.
 	Loai bo cac phan tu co gia tri trung nhau trong mang
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Nhap n
+	int n;
+	cout << "n = ";
+	cin >> n;
+	//Nhap mang
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	//
+	int k = 0, count, temp;
+	int arr1[n];
+	for(int i = 0; i < n; i++){
+		count = 0;
+		temp = arr[i];
+		for(int j = i + 1; j < n; j++){
+			if(arr[i] == arr[j]){
+				break;
+			}
+		}
+		for(int m = 0; m < k; m++){
+			if(temp == arr1[m]){
+				count++;
+			}
+		}
+		if(count == 0){
+			arr1[k] = arr[i];
+			k++;
+		}
+	}
+	//Hien thi
+	for(int i = 0; i < k; i++){
+		cout << arr1[i] << "\t";
+	}
+	return 0;
+}
--- Buoi9/Bai16.cpp
+++ Buoi9/Bai16.cpp
@@ -4,3 +4,5 @@
 		- 1 mang chua cac so nguyen to
 		- 1 mang chua cac so khong phai so nguyen to
 */
+
+
