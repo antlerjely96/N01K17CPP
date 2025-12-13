8b18fedd6c45cd0487be615d0253259f9a689cb2
antlerjely96 <antlerjely96@gmail.com>
2025-12-13 11:43:54 +0700

    Function Bai4

--- Buoi11/Bai4.cpp
+++ Buoi11/Bai4.cpp
@@ -1,3 +1,25 @@
 /* 
 	4. Nhap 1 so nguyen, kiem tra so nguyen do la am hay duong.
 */
+#include <iostream>
+using namespace std;
+
+int nhap(){
+	int n;
+	cin >> n;
+	return n;
+}
+void kiemTra(int a){
+	if(a < 0){
+		cout << a << " la so am" << endl;
+	} else if(a > 0){
+		cout << a << " la so duong" << endl;
+	} else {
+		cout << a << " la so khong am khong duong" << endl;
+	}
+	return;
+}
+int main(){
+	int a = nhap();
+	kiemTra(a);
+}
