c47717c90d9c61433846f330d8034dcbf9dbd264
antlerjely96 <antlerjely96@gmail.com>
2025-12-13 09:47:35 +0700

    Function Bai2

---
+++ Buoi11/Bai2.cpp
@@ -0,0 +1,11 @@
+/*
+	2. Nhap 3 so nguyen x, y, z. Tính
+		a. x + y + z
+		b. x * y * z
+		c. x^2 + y^2 - z^2
+		d. x^3 / y^2 * z
+		e. x / y / z
+		f. (x + y) - (x + z) - (y + z)
+		g. (x * y) / (x + y + z)
+		h. (x + y + z) / 3
+*/
